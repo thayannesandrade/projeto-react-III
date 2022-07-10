55a7cf031f5bbe10802e313a6d15b0f0ffc3fc4f
Thayanne Andrade <thayannesandrade@gmail.com>
2022-07-10 16:51:52 -0300

    adicionando estrutura das páginas da aplicação

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ApplicationRoutes from "./routes/ApplicationRoutes"
+
+function App() {
+  return(
+    <ApplicationRoutes/>
+  )
+}
+
+export default App
+
---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header/Header'
+import image from '../../assets/project1.svg'
+
+
+function Portfolio() {
+    return(
+        <Header image={image} description="">
+            Meus Projetos
+            </Header>
+    )
+}
+
+export default Portfolio
